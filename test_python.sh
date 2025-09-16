130ae55c91f50618aa36b2d0ebfa92648fe18125
Razvan Deaconescu <razvand@unikraft.io>
2025-09-17 01:59:11 +0300

    test: Use URLs as parameters
    Use a host URL as a parameter for the JavaScript and Python tests. This
    removes the hardcoded URL and gives flexibility to running tests.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- test_python.sh
+++ test_python.sh
@@ -1,6 +1,16 @@
-#!/bin/bash 
+#!/bin/sh
 
-curl -X POST http://localhost:4000/run \
+if test $# -ne 1; then
+    echo "Usage: $0 <runner-url>" 1>&2
+    echo "" 1>&2
+    echo "    $0 http://localhost:4000" 1>&2
+    echo "    $0 https://code-runner-magicloops.fra0.kraft.host" 1>&2
+    exit 1
+fi
+
+runner_url="$1"
+
+curl -s -X POST "$runner_url"/run \
   -H 'Content-Type: application/json' \
   -d '{
         "image": "python:latest",
